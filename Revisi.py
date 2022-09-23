60d3bb81d162715ec24cf9a857c00b22fb4b49a4
ZaqyD <114204008+ZaqyD@users.noreply.github.com>
2022-09-23 10:38:16 +0700

    Update Revisi.py

--- Revisi.py
+++ Revisi.py
@@ -1,54 +1,8 @@
 jenis=input("Pilih Jenis Laporan [Kehilangan/Fasilitas Umum(FU)] :")
-if jenis=="Kehilangan" or jenis=="kehilangan":
-    nik=input("Masukan NIk Anda : ")
-    nama=input("Masukkan Nama Anda : ")
-    import datetime
-    tanggal = datetime.datetime.now()
-    alamat=input("Alamat : ")
-    hilang=input("Laporan Kehilangan : ")
-
-
-    print ("====================================================================")
-    print ("NIK yang Anda Masukkan adalah : "+nik)
-    print ("Nama yang Anda Masukkan adalah : "+nama)
-    print("Tanggal Laporan :" , tanggal)
-    # print ("Tanggal yang Anda Masukkan adalah : "+tanggal)
-    print ("Alamat yang Anda Masukkan adalah : "+alamat)
-    print ("Laporan yang Anda Masukkan adalah : "+hilang)
-    print ("====================================================================")
-
-    check=input("""Tulis "Benar" jika input benar Tulis "Salah" Jika sebaliknya :""")
-    if check == "Benar" or check =="benar":
-        print("Selesai")
-    elif check == "Salah" or check == "salah":
-        print()
-    else:
-        print("Coba Input Ulang")
-
-elif jenis=="Fasilitas Umum" or jenis=="FU":
-    nik=input("Masukan NIk Anda : ")
-    nama=input("Masukkan Nama Anda : ")
-    import datetime
-    tanggal = datetime.datetime.now()
-    alamat=input("Alamat : ")
-    fu=input("Laporan Fasilitas Umum : ")
-
-
-    print ("====================================================================")
-    print ("NIK yang Anda Masukkan adalah : "+nik)
-    print ("Nama yang Anda Masukkan adalah : "+nama)
-    print("Tanggal Laporan :" , tanggal)
-    # print ("Tanggal yang Anda Masukkan adalah : "+tanggal)
-    print ("Alamat yang Anda Masukkan adalah : "+alamat)
-    print ("Laporan yang Anda Masukkan adalah : "+fu)
-    print ("====================================================================")
+if jenis=="Kehilangan" or jenis=="kehilangan":   
+    hilang="Kehilangan"
     
-    check=input("""Tulis "Benar" jika input benar Tulis "Salah" Jika sebaliknya :""")
-    if check == "Benar" or check =="benar":
-        print("Selesai")
-    elif check == "Salah" or check == "salah":
-        print()
-    else:
-        print("Coba Input Ulang")
+elif jenis=="Fasilitas Umum" or jenis=="FU":
+    fu="Fasilitas Umum"
 else:
     print("Tidak ada yang di pilih")
